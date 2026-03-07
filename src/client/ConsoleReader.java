c3ec7bdd98b04fcc108a2371d3379a62c0c783c9
Даниил Иоффе <6k1ttles@niuitmo.ru>
2026-03-07 15:27:07 +0300

    reader

---
+++ src/client/ConsoleReader.java
@@ -0,0 +1,10 @@
+package client;
+
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import java.io.IOException;
+
+public class ConsoleReader {
+}
