191deb45d30fd7617e448c619caa7ab8adcaeaea
Alex Nolan <505452@niuitmo.ru>
2026-03-10 11:44:33 +0300

    added

---
+++ src/server/commands/ExitCommand.java
@@ -0,0 +1,19 @@
+package server.commands;
+
+import common.Request;
+import common.Response;
+
+public class ExitCommand implements Command{
+
+    @Override
+    public Response execute(Request request){
+        return new Response(true, "выход из программы");
+    }
+
+    @Override
+    public String getName(){return "exit";}
+
+    @Override
+    public String getDescription(){return "выходит из программы";}
+
+}
