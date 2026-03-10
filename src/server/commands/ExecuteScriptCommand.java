ec679fb020c42c69ad14a4463485f1f7ca3adc91
Alex Nolan <505452@niuitmo.ru>
2026-03-10 11:57:57 +0300

    added

---
+++ src/server/commands/ExecuteScriptCommand.java
@@ -0,0 +1,32 @@
+package server.commands;
+
+import common.Request;
+import common.Response;
+
+public class ExecuteScriptCommand implements Command{
+    private static java.util.Vector<String> activeScripts = new java.util.Vector<>();
+
+    @Override
+    public Response execute(Request request){
+        String fileName = request.getStringArgument();
+        if(activeScripts.contains(fileName)){
+            return new Response(false, "ошибка: бесконечная рекурсия");
+        }
+            return new Response(true,"запущен" + fileName);
+    }
+
+    @Override
+    public String getName(){
+        return "execute_script";
+    }
+
+    @Override
+    public String getDescription(){
+        return "выполняет скрипт";
+    }
+
+    public static void removeScript(String fileName){
+        activeScripts.remove(fileName);
+    }
+
+}
